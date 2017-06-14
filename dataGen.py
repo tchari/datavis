97b7e7f583d776c67100141280c0534ee9d05b77
Tarun Chari <tchari@ACDM.DS.SAIT.CA>
2017-06-14 13:32:17 -0600

    June 14 2017, 1:31 pm

--- dataGen.py
+++ dataGen.py
@@ -1,13 +1,14 @@
 from random import randint
+numPoints = 10;
 filename = "data.txt"
 
 output = "["
-for i in range(0,10):
+for i in range(0,numPoints):
     output += "["
     for j in range(0,3):
         num = str(randint(0,100))
         output += num + ", "
-    if i == 9:
+    if i == numPoints-1:
         output += str(randint(0,100))+"]"
     else:
         output += str(randint(0,100))+"],"
